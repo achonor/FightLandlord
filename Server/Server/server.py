86dcff9ecd6e425a6b2724107d1fc6f3257df7d2
achonor <lidaha.004@hotmail.com>
2017-05-05 23:46:02 +0800

    优化代码结构

--- Server/Server/server.py
+++ Server/Server/server.py
@@ -12,14 +12,28 @@ class Server(object):
 
     #处理请求协议
     def requestServer(self, playerID, messageID, requestProto):
+        requestFunc = None
         protoType = type(requestProto)
         if (cmd_pb2.MessageLoginReq == protoType):
-            self.requestLogin(playerID, messageID, requestProto)
-        elif():
-            pass
+            requestFunc= self.requestLogin
+        elif(cmd_pb2.MessagePeopleReq == protoType):
+            requestFunc = self.requestPeople
+        if(None == requestFunc):
+            return
+        #处理数据
+        rProto = requestFunc(playerID, messageID, requestProto)
+        #发送数据
+        GGData.My_Factory.returnData(playerID, messageID, rProto)
 
     #登陆请求
-    def requestLogin(self,playerID, messageID,  proto):
+    def requestLogin(self, playerID, messageID,  proto):
         rProto = cmd_pb2.MessageLoginRsp()
         rProto.playerID = playerID
-        GGData.My_Factory.returnData( playerID , messageID, rProto)
+        return rProto
+
+    def requestPeople(self, playerID, messageID,  proto):
+        rProto = cmd_pb2.MessagePeopleRsp()
+        rProto.name = 'achonor'
+        rProto.phone = '18075952730'
+        rProto.age = 20
+        return rProto
