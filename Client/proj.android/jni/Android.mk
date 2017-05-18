bbb42a635e7c62aa316ccd01ff93bb82a5036279
achonor <lidaha.004@hotmail.com>
2017-05-18 18:41:32 +0800

    添加源文件

--- Client/proj.android/jni/Android.mk
+++ Client/proj.android/jni/Android.mk
@@ -24,6 +24,7 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/GameScene.cpp \
                    ../../Classes/GGText.cpp \
                    ../../Classes/GGData.cpp \
+                   ../../Classes/audioManage.cpp \
                    ../../Classes/checkPoker.cpp \
                    ../../Classes/functions.cpp \
                    ../../Classes/stdafx.cpp \
